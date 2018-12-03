75a1c8ab27fdf43d5e5703405e9dbe85e434062c
Charlie Say <charlie.say@autotrader.co.uk>
2018-12-03 14:05:38 +0000

    solution 1

---
+++ src/main/java/DAY1/Solution.java
@@ -0,0 +1,50 @@
+package DAY1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Solution {
+
+    private static List<String> getInput(){
+        List<String> listString = new ArrayList<>();
+        try{
+            File file = new File("/Users/charlie.say/PersonalProjects/CharlieSay/src/main/resources/DAY1/DAY1_INPUT.txt");
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String st;
+            while ((st = br.readLine()) != null){
+                listString.add(st);
+            }
+        }catch(Exception e){}
+        return listString;
+    }
+
+    public static int part1(List<Integer> streamableList) {
+        return streamableList.stream().reduce(0, Integer::sum);
+    }
+
+    public static int part2(List<Integer> integerList) {
+        Set<Integer> findUnique = new HashSet<>();
+        int frequency = 0;
+        while(true) {
+            for(int integer : integerList) {
+                frequency += integer;
+                if(!findUnique.add(frequency)) {
+                    return frequency;
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        List<Integer> numbersAsList = getInput().stream().map(Integer::parseInt).collect(Collectors.toList());
+        System.out.println(part1(numbersAsList));
+        System.out.println(part2(numbersAsList));
+    }
+}
---
+++ src/test/java/DAY1/SolutionTest.java
@@ -0,0 +1,13 @@
+package DAY1;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+
+
+}
